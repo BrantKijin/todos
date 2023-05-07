aa65664dfbd03cfe46e841f64f9c344afbfedfb0
BrantKijin <upper0house@gmail.com>
2023-05-08 00:52:23 +0900

    feat: 할일 목록 조회 API 제공하기(상)

---
+++ src/main/java/todoapp/TodoRestController.java
@@ -0,0 +1,24 @@
+package todoapp;
+
+
+import java.util.Collections;
+import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import todoapp.core.todos.application.TodoFinder;
+import todoapp.core.todos.domain.Todo;
+
+@RestController
+public class TodoRestController {
+
+  private final TodoFinder finder;
+
+  public TodoRestController(TodoFinder finder) {
+    this.finder = finder;
+  }
+
+  @RequestMapping("/api/todos")
+  public List<Todo> list(){
+    return finder.getAll();
+  }
+}
--- src/main/java/todoapp/web/TodoController.java
+++ src/main/java/todoapp/web/TodoController.java
@@ -1,12 +1,9 @@
 package todoapp.web;
 
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 import todoapp.web.model.SiteProperties;
 
 @Controller
