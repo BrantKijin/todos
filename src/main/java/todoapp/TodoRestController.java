c76ee14d8be90a7e4eb2ce150981a095a91bbad4
BrantKijin <upper0house@gmail.com>
2023-05-08 01:38:57 +0900

    feat: 할일 변경 API 제공하기1

--- src/main/java/todoapp/TodoRestController.java
+++ src/main/java/todoapp/TodoRestController.java
@@ -2,9 +2,14 @@ package todoapp;
 
 
 import java.util.List;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,14 +33,32 @@ public class TodoRestController {
     return finder.getAll();
   }
 
+  @PutMapping("/api/todos/{id}")
+  public void update(@PathVariable("id") Long id, @RequestBody WriteTodoCommand command){
+
+    editor.update(id, command.getTitle(), command.isCompleted());
+  }
+
+
   @PostMapping("/api/todos")
   @ResponseStatus(HttpStatus.CREATED)
-  public void create(@RequestBody CreateTodoCommand command){
+  public void create(@RequestBody @Valid CreateTodoCommand command){
     // JsonNode Map<String, Object>
     editor.create(command.getTitle());
   }
   static class CreateTodoCommand{
+    @NotBlank
+    @Size(min =4, max=140)
     private String title;
+    private Boolean completed;
+
+    public Boolean getCompleted() {
+      return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+      this.completed = completed;
+    }
 
     public String getTitle() {
       return title;
@@ -46,6 +69,37 @@ public class TodoRestController {
       return "CreateTodoCommand{" +
           "title='" + title + '\'' +
           '}';
+    }
+
+    public void setTitle(String title) {
+      this.title = title;
+    }
+  }
+
+  static class WriteTodoCommand{
+    @NotBlank
+    @Size(min =4, max=140)
+    private String title;
+    private boolean completed;
+
+    public boolean isCompleted() {
+      return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+      this.completed = completed;
+    }
+
+    @Override
+    public String toString() {
+      return "WriteTodoCommand{" +
+          "title='" + title + '\'' +
+          '}';
+    }
+
+    public String getTitle() {
+
+      return title;
     }
 
     public void setTitle(String title) {
