5f2f22db47698c9b2805ddbda872f01c8c7d06da
BrantKijin <upper0house@gmail.com>
2023-05-08 01:20:42 +0900

    feat: 할일 등록 API 제공하기(하)

--- src/main/java/todoapp/TodoRestController.java
+++ src/main/java/todoapp/TodoRestController.java
@@ -2,9 +2,13 @@ package todoapp;
 
 
 import java.util.List;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import todoapp.core.todos.application.TodoEditor;
 import todoapp.core.todos.application.TodoFinder;
 import todoapp.core.todos.domain.Todo;
 
@@ -12,11 +16,12 @@ import todoapp.core.todos.domain.Todo;
 public class TodoRestController {
 
   private final TodoFinder finder;
+  private final TodoEditor editor;
 
-  public TodoRestController(TodoFinder finder) {
+  public TodoRestController(TodoFinder finder, TodoEditor editor) {
     this.finder = finder;
+    this.editor = editor;
   }
-
 
   @GetMapping("/api/todos")
   public List<Todo> list(){
@@ -24,7 +29,27 @@ public class TodoRestController {
   }
 
   @PostMapping("/api/todos")
-  public void create(){
+  @ResponseStatus(HttpStatus.CREATED)
+  public void create(@RequestBody CreateTodoCommand command){
+    // JsonNode Map<String, Object>
+    editor.create(command.getTitle());
+  }
+  static class CreateTodoCommand{
+    private String title;
 
+    public String getTitle() {
+      return title;
+    }
+
+    @Override
+    public String toString() {
+      return "CreateTodoCommand{" +
+          "title='" + title + '\'' +
+          '}';
+    }
+
+    public void setTitle(String title) {
+      this.title = title;
+    }
   }
 }
