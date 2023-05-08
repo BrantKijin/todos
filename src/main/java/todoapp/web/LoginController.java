e4906c8d89ae4fc82a0cdf9881dad20a626837bf
BrantKijin <upper0house@gmail.com>
2023-05-08 20:35:38 +0900

    feat: 폼데이터 다루기

--- src/main/java/todoapp/web/LoginController.java
+++ src/main/java/todoapp/web/LoginController.java
@@ -4,12 +4,14 @@ package todoapp.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import todoapp.web.model.SiteProperties;
 
 @Controller
 public class LoginController {
+
+
 
   private final SiteProperties siteProperties;
 
@@ -26,6 +28,40 @@ public class LoginController {
   public ModelAndView loginForm(Model model){
 
     return new ModelAndView("/login");
+  }
+
+  @PostMapping("/login")
+  public void loginProcess(LoginCommand loginCommand){
+
+  }
+
+  static class LoginCommand {
+    String username;
+    String password;
+
+    @Override
+    public String toString() {
+      return "LoginCommand{" +
+          "username='" + username + '\'' +
+          ", password='" + password + '\'' +
+          '}';
+    }
+
+    public String getUsername() {
+      return username;
+    }
+
+    public void setUsername(String username) {
+      this.username = username;
+    }
+
+    public String getPassword() {
+      return password;
+    }
+
+    public void setPassword(String password) {
+      this.password = password;
+    }
   }
 
 }
